62689aa9f67668ecfb0ee923c6209da821761239
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 18:02:46 -0500

    modificacion de credito e historial

--- models/historial.py
+++ models/historial.py
@@ -1,10 +1,7 @@
 from sqlmodel import SQLModel, Field
-from datetime import datetime
 from typing import Optional
 
 class Historial(SQLModel, table=True):
-    idHistorial: Optional[int] = Field(default=None, primary_key=True)
-    entidad: str
+    id: Optional[int] = Field(default=None, primary_key=True)
     accion: str
-    descripcion: str
-    fecha: datetime = Field(default_factory=datetime.now)
+    detalle: str
