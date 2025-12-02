ff7d1fc1483572bed897a93cf8097832531e578f
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 18:22:07 -0500

    se mejora la estructura del codigo de main

--- main.py
+++ main.py
@@ -1,35 +1,36 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-from fastapi import Request
+from fastapi.staticfiles import StaticFiles
+
 from database import create_db_and_tables
-from routers.usuario_router import router as usuario_router
-from routers.credito_router import router as credito_router
-from routers.interes_router import router as interes_router
-from routers.historial_router import router as historial_router
-from routers.categoria_router import router as categoria_router
-from routers.reporte_router import router as reporte_router
-from routers.simulacion_router import router as simulacion_router
+from routers import (
+    usuario_router,
+    credito_router,
+    categoria_router,
+    historial_router,
+    interes_router,
+    simulacion_router,
+    reporte_router
+)
 
-app = FastAPI(title="Proyecto Banco")
+app = FastAPI(title="Simulador Créditos Bancarios")
 
-# ----- TEMPLATES Y ARCHIVOS ESTÁTICOS -----
-app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.on_event("startup")
+def startup_event():
+    create_db_and_tables()
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("home.html", {"request": request})
 
-# ----- REGISTRO DE ROUTERS -----
-app.include_router(usuario_router)
-app.include_router(credito_router)
-app.include_router(interes_router)
-app.include_router(historial_router)
-app.include_router(categoria_router)
-app.include_router(reporte_router)
-app.include_router(simulacion_router)
-
-@app.on_event("startup")
-def startup():
-    create_db_and_tables()
+# Routers
+app.include_router(usuario_router.router)
+app.include_router(credito_router.router)
+app.include_router(categoria_router.router)
+app.include_router(historial_router.router)
+app.include_router(interes_router.router)
+app.include_router(simulacion_router.router)
+app.include_router(reporte_router.router)
