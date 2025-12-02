86c38e11d90bea46d930c32837d675bf8469a506
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 16:57:32 -0500

    Mejora de codigo de models/usuarios

--- models/usuario.py
+++ models/usuario.py
@@ -2,9 +2,9 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional, List
 
 class Usuario(SQLModel, table=True):
-    idUsuario: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
-    ingresos: float
-    gastos: float
+    correo: str
+    telefono: str
 
     creditos: List["Credito"] = Relationship(back_populates="usuario")
