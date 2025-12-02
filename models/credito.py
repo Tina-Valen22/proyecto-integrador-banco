0db50fd1407482c11f11cb4279f4717e880c73f8
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 17:45:56 -0500

    nuevos arreglos de codigo

--- models/credito.py
+++ models/credito.py
@@ -1,14 +1,10 @@
 from sqlmodel import SQLModel, Field, Relationship
 from typing import Optional
-from typing import List
 
 class Credito(SQLModel, table=True):
-    idCredito: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     monto: float
-    plazo: int
-    tipo: str
+    descripcion: str
+    usuario_id: int = Field(foreign_key="usuario.id")
 
-    usuario_id: Optional[int] = Field(default=None, foreign_key="usuario.idUsuario")
     usuario: Optional["Usuario"] = Relationship(back_populates="creditos")
-
-    interes: Optional["Interes"] = Relationship(back_populates="credito")
