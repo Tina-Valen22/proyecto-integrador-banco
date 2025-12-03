9877d40a2543fc2d24959cbf58dc762964f75df0
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 19:28:08 -0500

    actualización de main y database

--- database.py
+++ database.py
@@ -1,15 +1,12 @@
-from sqlmodel import SQLModel, Session, create_engine
+from sqlmodel import SQLModel, create_engine
 
-DATABASE_URL = "sqlite:///./banco.db"
+DATABASE_URL = "sqlite:///./database.db"
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    echo=False,
+    connect_args={"check_same_thread": False}  # NECESARIO PARA RENDER
 )
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-
-def get_session():
-    with Session(engine) as session:
-        yield session
--- main.py
+++ main.py
@@ -1,36 +1,45 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
+from routers.usuario_router import router as usuario_router
+from routers.credito_router import router as credito_router
+from routers.categoria_router import router as categoria_router
+from routers.interes_router import router as interes_router
+from routers.simulacion_router import router as simulacion_router
+from routers.reporte_router import router as reporte_router
+from routers.historial_router import router as historial_router
+
 from database import create_db_and_tables
-from routers import (
-    usuario_router,
-    credito_router,
-    categoria_router,
-    historial_router,
-    interes_router,
-    simulacion_router,
-    reporte_router
-)
 
-app = FastAPI(title="Simulador Créditos Bancarios")
 
+app = FastAPI(title="Banco - Sistema de Simulación")
+
+# Templates
 templates = Jinja2Templates(directory="templates")
+
+# Static
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+
+# Crear BD al iniciar
 @app.on_event("startup")
 def startup_event():
     create_db_and_tables()
 
-@app.get("/")
+
+# Página principal (home)
+@app.get("/", response_class=HTMLResponse)
 def home(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
+
 # Routers
-app.include_router(usuario_router.router)
-app.include_router(credito_router.router)
-app.include_router(categoria_router.router)
-app.include_router(historial_router.router)
-app.include_router(interes_router.router)
-app.include_router(simulacion_router.router)
-app.include_router(reporte_router.router)
+app.include_router(usuario_router)
+app.include_router(credito_router)
+app.include_router(categoria_router)
+app.include_router(interes_router)
+app.include_router(simulacion_router)
+app.include_router(reporte_router)
+app.include_router(historial_router)
