b22f2e7b54fa38d59ecd99531972053d4ca0bd6f
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-02 19:55:04 -0500

    se mejora el database

--- database.py
+++ database.py
@@ -1,12 +1,27 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import SQLModel, create_engine, Session
+import os
 
-DATABASE_URL = "sqlite:///./database.db"
+# --- CONFIGURAR BASE DE DATOS PARA RENDER O LOCAL ---
 
-engine = create_engine(
-    DATABASE_URL,
-    echo=False,
-    connect_args={"check_same_thread": False}  # NECESARIO PARA RENDER
-)
+# Si Render define DATABASE_URL, úsala. Si no, usa SQLite local.
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./banco.db")
 
+# Cuando es SQLite en Render, debe usar connect_args especiales.
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
+
+# --- CREAR TABLAS ---
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
+
+# --- SESIONES ---
+def get_session():
+    with Session(engine) as session:
+        yield session
