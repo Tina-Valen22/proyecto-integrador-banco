291bb0ecbef42bbf49d0fd880793c59d86224535
Valentina Ovalle Osorio <vovalle72@ucatolica.edu.co>
2025-12-03 20:49:57 -0500

    Corrección main.py

--- main.py
+++ main.py
@@ -30,6 +30,9 @@ app = FastAPI(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 # -----------------------------
 # Eventos de ciclo de vida
